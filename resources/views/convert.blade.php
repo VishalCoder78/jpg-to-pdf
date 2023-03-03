e7c8520ccc19cc74c08187046944120aec2ad7b0
VishalCoder78 <vishalcoder78@gmail.com>
2023-03-03 14:15:03 +0530

    commit

--- resources/views/convert.blade.php
+++ resources/views/convert.blade.php
@@ -8,12 +8,19 @@
     <title>convert</title>
 </head>
 <body>
-    <div class="h-screen w-screen flex items-center justify-center  ">
-        <form action="{{url('/convert')}}" method="POST" enctype="multipart/form-data" class="flex-row">
+    <x-navbar />
+    <div class="text-center">
+        <h1 class="text-4xl font-bold">JPG TO PDF</h1>
+        <h4 class="text-xl">Convert JPG images to PDF in seconds. Easily adjust orientation and margins.</h4>
+
+    </div>
+    <div class="h-[30rem] w-screen flex justify-center  ">
+        <form action="{{url('/convert')}}" method="POST" enctype="multipart/form-data" class="grid grid-cols-5 grid-rows-6 gap-4 h-96 w-screen " >
             @csrf
-            <input type="file" name="file">
-            <button type="submit" class=" bg-emerald-600 hover:bg-emerald-800 text-white px-4 py-1">Convert into Image</button>
+            <input type="file" name="file" class=" col-start-1 col-span-5 row-start-1 row-span-5 border border-black bg-neutral-300 form-control center">
+            <button type="submit" class=" bg-emerald-600 hover:bg-emerald-800 text-white w-52 col-start-3  col-span-3 row-span-1" >Convert into PDF</button>
         </form>
-            </div>
+    </div>
+    <x-footer />
 </body>
 </html>
--- resources/views/viewpdf.blade.php
+++ resources/views/viewpdf.blade.php
@@ -8,8 +8,11 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="h-screen flex items-center justify-center bg-slate-800">
+    <x-navbar />
+    <div class="h-screen flex flex-col items-center justify-center bg-slate-800">
+        <h1 class="text-4xl text-white m-5">The images have been converted into PDF</h1>
         <a href="/download/<?php echo $path ?>" class="bg-emerald-600 hover:bg-emerald-400 p-4 px-6 rounded-lg" >Download PDF</a>
     </div>
+    <x-footer />
 </body>
 </html>
